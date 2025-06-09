13081874b6059a4fe24d38b5fd97d3adfb178de5
dekbadnerd <Shelikedeknerd.777@gmail.com>
2025-06-10 01:17:13 +0700

    Backend setup
    - Connect to MongoDB
    - Auth Routes(signup, login, logout, update profile)
    - jwt and stuff...

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import { connectDB } from "./lib/db.js"
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config()
+const port = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT:${port}`)
+    connectDB()
+})
